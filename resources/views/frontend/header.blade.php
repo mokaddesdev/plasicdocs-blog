a889c5993f989bccdbf9fc144d724146d460c17f
Mokaddes Ali <mokaddes.ru2000@gmail.com>
2026-05-12 22:42:37 +0600

    code review and solve error hompage

--- resources/views/frontend/header.blade.php
+++ resources/views/frontend/header.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>@yield('title', 'PlasicDocs')</title>
 
       <!-- Fonts -->
   <link rel="preconnect" href="https://fonts.googleapis.com" />
--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.app')
 
+
 @section('content')
 
 @include('frontend.home.hero')
