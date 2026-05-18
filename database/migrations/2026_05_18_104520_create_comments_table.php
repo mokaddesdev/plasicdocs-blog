ba546ba0e3fc4d57f696278b8bfd34b545eea27d
mokaddesdev <mokaddes.ru2000@gmail.com>
2026-05-18 17:26:51 +0600

    add newslatter sbscribers and comments table

---
+++ database/migrations/2026_05_18_104520_create_comments_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('post_id')
+                  ->constrained('posts')
+                  ->onUpdate('cascade')
+                  ->onDelete('cascade');
+            $table->foreignId('parent_id')
+                  ->constrained('comments')
+                  ->nullable()
+                  ->onUpdate('cascade')
+                  ->onDelete('cascade');
+            $table->foreignId('user_id')
+                  ->constrained('users')
+                  ->onUpdate('cascade')
+                  ->onDelete('cascade');
+            $table->string('author_name', 255)->nullable();
+            $table->string('author_email', 255)->nullable();
+            $table->text('content');
+            $table->boolean('is_approve')->default(false);
+            $table->string('ip_address', 45)->nullable();
+            $table->text('user_agent')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comments');
+    }
+};
